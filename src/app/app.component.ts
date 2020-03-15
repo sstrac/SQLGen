abb0b2860b60b7479b151381def97bda73feba58
sstrac <s.r.strachan@outlook.com>
2020-03-15 23:45:41 +0000

    cleanup working properly for all tables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, Output, Input } from '@angular/core';
 import { Entity, JOB_VACANCY_KEYS, JOB_KEYS, PROFILE_KEYS, TABLE_MAPPING } from './shared/table';
-import { Observable, of } from 'rxjs';
+import { Observable, of, observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,42 +10,57 @@ import { Observable, of } from 'rxjs';
 export class AppComponent {
   tables: Entity[] = []
   tableKeys: Entity[] = []
-  observable: Observable<Entity>
-  obs1 = of(this.tables[1])
+  observables: Entity[] = []
 
   constructor() {
     this.tables.push(
-      { name: 'job_vacancy', fields: {} },
-      { name: 'job', fields: {} }
+      { name: 'job_vacancy', fields: { job_id: ''} },
+      { name: 'job', fields: { id: '' } }
     )
     this.tableKeys.push(
       JOB_VACANCY_KEYS, JOB_KEYS
     )
+    this.tables.forEach( table => {
+      this.observables.push(
+        { name: table.name, fields: of(table) }
+        )
+    });
   }
 
   ngOnInit(){
 
-
   }
+
   sendValues(event, tablename){
+    //only handle one table
     var cols = this.getEntityByName(tablename, this.tableKeys).fields
     let table = this.getEntityByName(tablename, this.tables)
-    let valueArr = event.target.value.split(",")
+    let valueArr = event.target.value.split(',')
     for (let i = 0; i < valueArr.length; i++) {
-      table.fields[cols[i]] = valueArr[i]
+      if( i < cols.length)
+        table.fields[cols[i]] = valueArr[i]
     }
-    this.cleanupEmptyValues(table)
-    this.overrideChildValues()
-    console.log(this.tables)
-    this.observable = of(table)
+    console.log(valueArr)
+    this.overrideChildValues() //handle update to child linked tables
+    //should send updates to all table entities
+    this.cleanupEmptyValues() //cleansup all tables
+    this.updateTables()
   }
 
-  cleanupEmptyValues(table){
-    for(const field in table.fields) {
-      if(table.fields[field] === ''){
-        delete table.fields[field]
-      }
+  updateTables(){
+    for(let i = 0; i < this.tables.length; i++){
+      this.observables[i].fields = of(this.tables[i])
     }
+  }
+
+  cleanupEmptyValues(){
+    this.tables.forEach(table => {
+      for(const field in table.fields) {
+        if(table.fields[field] === ''){
+          delete table.fields[field]
+        }
+      }
+    });
   }
 
   overrideChildValues(){
