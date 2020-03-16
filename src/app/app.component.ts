cc694ee79a09bf79571f88fd3ee20275bd4b0117
sstrac <s.r.strachan@outlook.com>
2020-03-16 03:32:06 +0000

    random generator, clear fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,26 +19,44 @@ export class AppComponent {
     this.tables.push(
       { name: 'job_vacancy', fields: {} },
       { name: 'job', fields: {} },
-      { name: 'profile', fields: {}}
+      { name: 'profile', fields: {} }
     )
-    this.tables.forEach( table => {
+    this.tables.forEach(table => {
       this.observables.push(
         { name: table.name, fields: of(table) }
-        )
+      )
     });
   }
 
-  ngOnInit(){
+  ngOnInit() {
 
   }
 
-  sendValues(event, tablename){
+  generateRandomValues() {
+    this.tableKeys.forEach(tableKey => {
+      let values: string = ""
+      for (let i = 0; i < tableKey.fields.length; i++) {
+        values += makeid(5) + ","
+      }
+      let event: any = { target: { value: values } }
+      this.sendValues(event, tableKey.name)
+    })
+  }
+
+  clearAllValues() {
+    this.tables.forEach(table => {
+      table.fields = {}
+    })
+  }
+
+  sendValues(event, tablename) {
     //only handle one table
     var cols = this.getEntityByName(tablename, this.tableKeys).fields
     let table = this.getEntityByName(tablename, this.tables)
     let valueArr = event.target.value.split(',')
     for (let i = 0; i < valueArr.length; i++) {
-      if( i < cols.length)
+      if (i < cols.length)
+      //TODO: this can't handle bulk changes, only changes to single fields
         table.fields[cols[i]] = valueArr[i]
     }
     this.overrideChildValues() //handle update to child linked tables
@@ -47,47 +65,56 @@ export class AppComponent {
     this.updateTables()
   }
 
-  updateTables(){
-    for(let i = 0; i < this.tables.length; i++){
+  updateTables() {
+    for (let i = 0; i < this.tables.length; i++) {
       this.observables[i].fields = of(this.tables[i])
     }
   }
 
-  cleanupEmptyValues(){
+  cleanupEmptyValues() {
     this.tables.forEach(table => {
-      for(const field in table.fields) {
-        if(table.fields[field] === ''){
+      for (const field in table.fields) {
+        if (table.fields[field] === '') {
           delete table.fields[field]
         }
       }
     });
   }
 
-  overrideChildValues(){
+  overrideChildValues() {
     TABLE_MAPPING.forEach(mapping => {
       let parentTable = this.getEntityByName(mapping.parentEntityName, this.tables)
       let childTable = this.getEntityByName(mapping.childEntityName, this.tables)
-      if(parentTable.name == mapping.parentEntityName
+      if (parentTable.name == mapping.parentEntityName
         && childTable.name == mapping.childEntityName
-        && isDefined(childTable.fields[mapping.childKey])){
-          parentTable.fields[mapping.parentKey] = childTable.fields[mapping.childKey]
-        }
+        && isDefined(childTable.fields[mapping.childKey])) {
+        parentTable.fields[mapping.parentKey] = childTable.fields[mapping.childKey]
+      }
     });
   }
 
   getEntityByName(itemName: string, entities: Entity[]): Entity {
     let result
     entities.forEach(item => {
-      if(item.name == itemName){
+      if (item.name == itemName) {
         result = item
       }
     });
-    if(result == undefined)
+    if (result == undefined)
       throw Error('item not found in entity array')
     return result;
   }
 
 }
-export function isDefined(item){
+export function isDefined(item) {
   return item !== undefined
+}
+function makeid(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
