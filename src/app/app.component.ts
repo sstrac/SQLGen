44b520a9490d32d049315b98dc6314c7fefca793
sstrac <s.r.strachan@outlook.com>
2020-03-17 23:05:35 +0000

    working ngModel, all tables, inserts working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { Component, Output } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit, Output } from '@angular/core';
 import * as TABLES from '../assets/tables.json'
-import * as TABLE_MAPPING from '../assets/table_mapping.json'
-import { Table, Mapping } from './shared/table.interface';
 import { EventEmitter } from '@angular/core';
 
 @Component({
@@ -10,90 +7,17 @@ import { EventEmitter } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  myControl = new FormControl();
-  tables: Table[] = []
-  tableMappings: Mapping[] = []
-  @Output() tableUpdate: EventEmitter<Table> = new EventEmitter<Table>()
+export class AppComponent implements OnInit{
+  key = 'keys'
+  tables = TABLES.default.tables
 
-  constructor() {
-    TABLES.default.tables.forEach(table => {
-      this.tables.push(table)
-    });
+  ngOnInit(){
   }
-
-  // generateRandomValues() {
-  //   this.tableKeys.forEach(tableKey => {
-  //     let values: string = ""
-  //     for (let i = 0; i < tableKey.fields.length; i++) {
-  //       values += makeid(5) + ","
-  //     }
-  //     let event: any = { target: { value: values } }
-  //     this.sendValues(event, tableKey.name)
-  //   })
-  // }
-
-  sendValue(event, tablename, fieldkeyname) {
-    this.tables.forEach(table => {
-      if (table.name === tablename) {
-        table.fieldKeys.forEach(fieldKey => {
-          if(fieldKey === fieldkeyname){
-            table.fields[fieldKey] = event.target.value
-          }
-        });
-      }
-    })
-    this.overrideChildValues()
-    this.cleanupEmptyValues()
-    this.tableUpdate.emit(this.tables[0])
-  }
-
-
-  overrideChildValues() {
-    this.tableMappings.forEach(mapping => {
-      let sourceTable = this.getTableByName(mapping.sourceTableName)
-      let destTable = this.getTableByName(mapping.destTableName)
-      if (sourceTable.name == mapping.sourceTableName
-        && destTable.name == mapping.destTableName
-        && isDefined(sourceTable.fields[mapping.sourceTableKey])) {
-        destTable.fields[mapping.destTableKey] = sourceTable.fields[mapping.sourceTableKey]
-      }
-    });
-  }
-
-  cleanupEmptyValues() {
-    this.tables.forEach(table => {
-      for (const field in table.fields) {
-        if (table.fields[field] === '') {
-          delete table.fields[field]
-        }
-      }
-    });
-  }
-
-  getTableByName(tablename: string) {
-    let result
-    this.tables.forEach(table => {
-      if (table.name == tablename) {
-        result = table
-      }
-    })
-    if (result == undefined)
-      throw Error('table not found among table objects')
-    return result;
-  }
-
-  clearAllValues = () => {
-    this.tables.forEach(table => {
-      table.fields = {}
-    })
-  }
-
 }
-export function isDefined(item) {
+export function isDefined(item): boolean {
   return item !== undefined
 }
-function makeid(length) {
+function makeid(length): string {
   var result = '';
   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatSidenavModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatButtonModule, MatIconModule, MatAutocompleteModule, MatFormField, MatOptionModule, MatDividerModule, MatCardModule } from '@angular/material'
-import { InsertComponent } from './insert/insert.component';
+import { MatSidenavModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatButtonModule, MatIconModule, MatAutocompleteModule, MatFormField, MatOptionModule, MatDividerModule, MatCardModule, MatSelectModule } from '@angular/material'
 import { IsEmptyPipe } from './shared/isempty.pipe';
 import { GroupComponent } from './shared/group.component';
 import { SidenavContentComponent } from './sidenav-content/sidenav-content.component';
+import { KeyValueArrPipe } from './shared/keyvaluearr.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InsertComponent,
     GroupComponent,
     SidenavContentComponent,
-    IsEmptyPipe
+    IsEmptyPipe,
+    KeyValueArrPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
-    MatSidenavModule,
-    MatFormFieldModule,
-    MatInputModule,
     MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatAutocompleteModule,
     MatFormFieldModule,
-    MatOptionModule,
     MatInputModule,
-    MatDividerModule,
-    MatCardModule
+    MatOptionModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
